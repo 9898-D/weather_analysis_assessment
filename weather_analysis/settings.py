e61bcd6cfa7dffe8c545ce506cbf57350a92ce39
Dhruv <choubisadh2000.dc@gmail.com>
2024-08-28 17:59:31 +0530

    Allow all IPs to access app

--- weather_analysis/settings.py
+++ weather_analysis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h53e0o70kl+u+$#yyymd!3=dq!j36dks&^!g_r)vo=$6d)637(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
